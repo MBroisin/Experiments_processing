6e71df458a96edf4d10831a29d2c38b897edfeb1
MBroisin <matthieu.broisin@epfl.ch>
2023-04-24 16:04:59 +0200

    activity: version with video save

--- activity_experiments_python/bee_activity.py
+++ activity_experiments_python/bee_activity.py
@@ -1,17 +1,43 @@
 import numpy
 import cv2
+import os 
 
 import matplotlib
 import matplotlib.pyplot
 import matplotlib.patches
 
+# VIDEO CREATION
+def video_generation(images_list, video_folder='./', video_name='test_vid.mp4', fps=30):
+    print('Saving video results in ' + video_folder + video_name)
+    
+    if (video_name in os.listdir(video_folder)):
+        os.system('rm {}'.format(video_folder+video_name))
+        
+    height, width = images_list[0].shape[:2]
+    
+    # video = cv2.VideoWriter(video_folder+video_name, cv2.VideoWriter_fourcc('a','v','c','1'), fps, (width,height))
+    video = cv2.VideoWriter(video_folder+video_name, cv2.VideoWriter_fourcc('m','p','4','v'), fps, (width,height))
+    #video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('X','V','I','D'), fps, (width,height))
+    
+    for i in range(0, len(images_list)):
+        if (len(images_list[i].shape) == 3):
+            video.write(numpy.uint8(images_list[i][:,:,[2,1,0]]))
+        else :
+            video.write(numpy.uint8(images_list[i]))
 
-def compute_video_activity(video, threshold=15, cropX=None, cropY=None, frames='all'):
+    cv2.destroyAllWindows()
+    video.release()
+    del video
+
+def compute_video_activity(video, threshold=15, cropX=None, cropY=None, frames='all', video_save=False, video_filename=None, video_foldername=None):
     
     cap = cv2.VideoCapture(video)
     
     prev_frame = numpy.array([])
     activities = numpy.zeros(20000)
+
+    if video_save:
+        vid = [[]]*20000
     
     if frames=='all':
         stop_frame=2000000000
@@ -52,6 +78,15 @@ def compute_video_activity(video, threshold=15, cropX=None, cropY=None, frames='
         activity = activity/(res_frame.shape[0]*res_frame.shape[1])
         activities[frame_count-1] = activity
         prev_frame = res_frame
+
+        if video_save :
+            vid[frame_count-1] = frame_crop
+            vid[frame_count-1][:,:,0]=numpy.maximum(vid[frame_count-1][:,:,0], numpy.uint8(act)*255)
+
+    if video_save:
+        vid = [v for v in vid if not(v==[])]
+        video_generation(vid, video_folder=video_foldername, video_name=video_filename, fps=30)
+    
     return list(activities[:(frame_count-2)])
 
 
